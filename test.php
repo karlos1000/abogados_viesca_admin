f3ad2771ffaec26af116ff291cbaef88d32accb7
Carlos JR <karlos.0@live.com.mx>
2021-01-07 20:26:31 -0600

    Se agrego clase para leer eventos de google por archivos ics

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'brules/usuariosObj.php';
+/* include_once 'brules/usuariosObj.php';
 $userObj = new usuariosObj();
 
 echo "Hola mundo<br>";
@@ -18,4 +18,16 @@ echo "nombre: ".$user->nombre."<br>";
 $user = $userObj->UserByID(6);
 echo "<pre>";print_r($user);echo"</pre>";
 echo "nombre: ".$user->nombre."<br>";
-echo "fin<br>";
+echo "fin<br>"; */
+
+include_once './libs/eventosCal/iCalEasyReader.php';
+$ical = new iCalEasyReader();
+$idCal = "q2n3jg9a0luaih80cahghksgu4@group.calendar.google.com"; //Identificador del calendario de google
+$arrEvents = $ical->getEventsCols($idCal); //Coleccion de eventos
+
+// $calData = $ical->load( file_get_contents( 'https://calendar.google.com/calendar/ical/'.$idCal.'/public/basic.ics' ) );
+
+echo "<pre>";
+print_r($arrEvents);
+// print_r($calData);
+echo "</pre>";
